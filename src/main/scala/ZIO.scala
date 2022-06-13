b089fb9bb8a3f1094e27190a6198028e3febad67
Katherine <katherine-fu@outlook.com>
2022-06-13 16:07:23 +0200

    commit

--- src/main/scala/ZIO.scala
+++ src/main/scala/ZIO.scala
@@ -4,7 +4,22 @@ import scala.util.matching.Regex
 case class Address(street: String, house_number: Option[String] = None, house_number_addition: Option[String] = None)
 
 object Address extends App {
-  def addressParting(address: Address) : Address = ???
+  val input: List[Address] =
+    List(
+      Address("PLEIN 1921"),
+      Address("WILLEM 2 STRAAT"),
+      Address("boeier 01 19A"),
+      Address("Willem 2 straat 19"),
+      Address("Veilinghavenkade Tweede Derde 129"),
+      Address("Veilinghavenkade Tweede 129B"),
+      Address("Veilinghavenkade 129-B"),
+      Address("Veilinghavenkade 129 1234"),
+      Address("Veilinghavenkade 129-1234"),
+      Address("Plein 20 0"),
+      Address("Archipel 11"),
+      Address("Archipel 12"),
+      Address("19 Onzinlaan")
+    )
 
   val rules: List[Regex] =
     Source
@@ -13,14 +28,8 @@ object Address extends App {
       .map(line => ("(?i)" + line).r)
       .toList
 
-  val input: List[String] =
-    Source
-      .fromFile("src/main/scala/test.txt")
-      .getLines
-      .toList
-
-  def filterAddressesWithDigits(regexes: List[Regex], addresses: List[String]): (List[String], List[String]) =
-    addresses.partition(address => regexes.exists(_.findFirstIn(address) != None))
+  def filterAddressesWithDigits(regexes: List[Regex], addresses: List[Address]): (List[Address], List[Address]) =
+    addresses.partition(address => regexes.exists(_.findFirstIn(address.street) != None))
 
   val (matched, unmatched) = filterAddressesWithDigits(rules, input)
 
