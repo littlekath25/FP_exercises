14f6ad95e7560b894214f2620404c1ffea9f6f52
Katherine <katherine-fu@outlook.com>
2022-06-13 15:54:24 +0200

    commit

--- src/main/scala/ZIO.scala
+++ src/main/scala/ZIO.scala
@@ -6,23 +6,23 @@ case class Address(street: String, house_number: Option[String] = None, house_nu
 object Address extends App {
   def addressParting(address: Address) : Address = ???
 
-  val regexes: List[Regex] =
+  val rules: List[Regex] =
     Source
       .fromFile("src/main/scala/splitsstraathuisnr.ini")
       .getLines
       .map(line => ("(?i)" + line).r)
       .toList
 
-  println(regexes)
-
-  val addresses: List[String] =
+  val input: List[String] =
     Source
       .fromFile("src/main/scala/test.txt")
       .getLines
       .toList
 
-  val (matched, unmatched) =
+  def filterAddressesWithDigits(regexes: List[Regex], addresses: List[String]): (List[String], List[String]) =
     addresses.partition(address => regexes.exists(_.findFirstIn(address) != None))
+
+  val (matched, unmatched) = filterAddressesWithDigits(rules, input)
 
   println(s"MATCHED:\n$matched\n")
   println(s"UNMATCHED:\n$unmatched\n")
