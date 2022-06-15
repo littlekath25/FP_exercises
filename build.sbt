179d9ebeed2349626b250f4c02a925dc2b606e38
Katherine <katherine-fu@outlook.com>
2022-06-15 10:20:51 +0200

    commit

--- build.sbt
+++ build.sbt
@@ -10,5 +10,7 @@ lazy val root = project
 
     libraryDependencies += "org.scalameta"  %% "munit"        % "0.7.29" % Test,
     libraryDependencies += "dev.zio"        %% "zio"          % "2.0.0-M4",
-    libraryDependencies += "dev.zio"        %% "zio-streams"  % "2.0.0-M4"
+    libraryDependencies += "dev.zio"        %% "zio-streams"  % "2.0.0-M4",
+    libraryDependencies += "org.apache.avro" % "avro"         % "1.11.0",
+    libraryDependencies += "com.sksamuel.avro4s" %% "avro4s-core" % "5.0.0"
   )
--- src/main/scala/ZIO.scala
+++ src/main/scala/ZIO.scala
@@ -1,52 +1,74 @@
 import scala.io.Source
 import scala.util.matching.Regex
+import org.apache.avro.file.DataFileReader
+import org.apache.avro.generic.{GenericDatumReader, GenericRecord}
+import org.apache.avro.file.DataFileStream
+import java.io.InputStream
+import java.io.File
+import java.io.FileInputStream
+import com.sksamuel.avro4s.AvroInputStream
+import com.sksamuel.avro4s.AvroSchema
+import scala.jdk.CollectionConverters._
 
-case class Address(street: String, house_number: Option[String] = None, house_number_addition: Option[String] = None)
+final case class Address(street: String, house_number: Option[String] = None, house_number_addition: Option[String] = None, postal_code: Option[String] = None, city: String, country: String)
 
-object Address extends App {
-  val input: List[Address] =
-    List(
-      Address("PLEIN 1921"),
-      Address("WILLEM 2 STRAAT"),
-      Address("boeier 01 19A"),
-      Address("Willem 2 straat 19"),
-      Address("Veilinghavenkade Tweede Derde 129"),
-      Address("Veilinghavenkade Tweede 129B"),
-      Address("Veilinghavenkade 129-B"),
-      Address("Veilinghavenkade 129 1234"),
-      Address("Veilinghavenkade 129-1234"),
-      Address("Plein 20 0"),
-      Address("Archipel 11"),
-      Address("Archipel 12"),
-      Address("19 Onzinlaan")
-    )
+def addressIsInvalid(address: Address) : Boolean =
+    address.house_number == None &&
+    address.street != "N/A" &&
+    address.country == "NL"
 
-  val rules: List[Regex] =
+val rules: List[Regex] =
     Source
-      .fromFile("src/main/scala/splitsstraathuisnr.ini")
+      .fromResource("splitsstraathuisnr.ini")
       .getLines
       .map(line => ("(?i)" + line).r)
       .toList
 
+object Address extends App {
+  val schema = AvroSchema[Address]
+  val inputStream = AvroInputStream.data[Address].from(getClass.getResourceAsStream("address-testset-prod-uncompressed.avro")).build(schema)
+  val inputIterator = inputStream.iterator
+  val input = List.from(inputIterator).filter(addressIsInvalid) 
+  inputStream.close()
+
   // Addresss filter of addresses with a digit in the streetname
-  def filterAddressesWithDigits(regexes: List[Regex], addresses: List[Address]): (List[Address], List[Address]) =
-    addresses.partition(address => regexes.exists(_.findFirstIn(address.street) != None))
+  def filterAddressesWithDigits(rules: List[Regex], addresses: List[Address]): (List[Address], List[Address]) =
+    addresses.partition(address => rules.exists(_.findFirstIn(address.street) != None))
 
-  val (withDigits, withoutDigits) = filterAddressesWithDigits(rules, input)
-
-  // Address filter of the address streetname and the rest
-  def splitAddressHouseNumber(addresses: List[Address]): List[Address] =
-    addresses.map(address =>
-      val splitted: Array[String] = address.street.split("(?=\\d)", 2)
-      Address(splitted.head.trim, Some(splitted.tail.mkString)))
-
-  val solved = splitAddressHouseNumber(withoutDigits)
+  // Address splitting of house number and addition
+  def splitHouseNumberAndAddition(originalAddress: Address, houseNumber: String, delimiter: String): Address =
+    val splitted = houseNumber.split(delimiter, 2)
+    originalAddress.copy(house_number =  Some(splitted.head), house_number_addition = Some(splitted.tail.mkString))
 
   // Housenumber filter of the house number and house number addition
-  def splitHouseNumber(addresses: List[Address]): List[Address] =
-    ???
+  def extractAdditionFromHouseNumber(originalAddress: Address, houseNumber: String): Address =
+    if (houseNumber.contains(" "))
+      splitHouseNumberAndAddition(originalAddress, houseNumber, " ")
+    else if (houseNumber.contains("-"))
+      splitHouseNumberAndAddition(originalAddress, houseNumber, "-")
+    else
+      splitHouseNumberAndAddition(originalAddress, houseNumber, "(?=\\D)")
 
-  println(s"MATCHED:\n$withDigits\n")
-  println(s"UNMATCHED:\n$withoutDigits\n")
-  println(s"SOLVED:\n$solved\n")
+  // check if the address is valid for correction
+  def addressContainsHouseNumber(splitted: Array[String]): Boolean =
+    splitted.size == 2
+
+  // Address filter of the address streetname and the rest
+  def splitAddressHouseNumber(originalAddress: Address, streetName: String, houseNumber: String): Address =
+    if (houseNumber.contains(" ", "-") || !houseNumber.forall(_.isDigit))
+      extractAdditionFromHouseNumber(originalAddress.copy(street = streetName), houseNumber)
+    else
+      originalAddress.copy(street = streetName, house_number = Some(houseNumber))
+
+  def fixAddress(address: Address): Address =
+    val splitted = address.street.split("(?=\\d)", 2).map(_.trim)
+    if (addressContainsHouseNumber(splitted))
+      splitAddressHouseNumber(address, splitted.head, splitted(1))
+    else
+      address
+
+  val (withDigits, withoutDigits) = filterAddressesWithDigits(rules, input)
+  println("BEFORE: \n" + withoutDigits.take(20).mkString("\n"))
+  println("AFTER: \n" + withoutDigits.map(fixAddress).take(20).mkString("\n"))
+  // println(fixAddress(Address("Veilinghavenkade 9B", None, None, Some("3521AT"), "Utrecht", "NL")))
 }
