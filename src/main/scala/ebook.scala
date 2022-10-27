e336f17223b954a683e97f4b196d0f0bef8569a4
Katherine <katherine-fu@outlook.com>
2022-10-27 20:15:36 +0200

    commit

--- src/main/scala/ebook.scala
+++ src/main/scala/ebook.scala
@@ -1,5 +1,34 @@
 package ebook
+import scala.collection.immutable.VectorMap
 
 object Main extends App {
-  println("Hello World")
+  def wc(document: String): VectorMap[String, Int] = 
+    val docWithoutNewLines = replaceNewLinesWithSpaces(document)
+    val docWithoutFormatting = stripFormattingCharacters(docWithoutNewLines)
+    val docuWithoutDoubleSpaces = stripDoubleSpaces(docWithoutFormatting)
+    val listOfWords = convertToSequence(docuWithoutDoubleSpaces)
+    val lowerWords = convertToLowerCase(listOfWords)
+    val mapOfWords = convertToMap(lowerWords)
+
+  def replaceNewLinesWithSpaces(text: String): String =
+    text.replaceAll("\n", " ")
+
+  def stripFormattingCharacters(text: String): String = 
+    text.replaceAll(";", "")
+    .replaceAll(",", "")
+    .replaceAll("\\.", "")
+    .replaceAll("—", "")
+    .replaceAll("!", "")
+    .replaceAll("\\?", "")
+
+  def stripDoubleSpaces(text: String): String = 
+    text.replaceAll(" +", " ")
+
+  def convertToSequence(text: String): Seq[String] =
+    text.split(" ").filter(_.trim != "").toSeq
+
+  def convertToLowerCase(text: Seq[String]): Seq[String] = 
+    text.map(_.toLowerCase)
+
+  def convertToMap(text: Seq[String]): Map[String, Int] = ???
 }
